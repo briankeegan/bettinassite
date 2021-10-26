298af48116522f8e6685c37a74303edae668cacf
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2021-10-26 16:30:49 -0400

    image and title change to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,67 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 
+
 const X = 3;
-function Truck() {
+function Family() {
   return (
     <Image
-      src="/pics/truck.png"
-      alt="Bettina next to loaded truck"
-      width={1874 / X}
-      height={1412 / X}
+      src="/pics/IMG_9877.jpg"
+      alt="Bettina holding baby"
+      width={1300 / X}
+      height={1570 / X}
     />
   );
 }
 
 const ImageContainer = styled.div`
-  margin-top: 5em;
+  margin-top: 4em;
+  margin-bottom: 3em;
+`;
+
+const ParagraphContainer = styled.div`
+  padding-left: 25em;
+  padding-right: 25em;
+  text-align: justify;
+  margin-bottom: 10em;
 `;
 
 const UnderConstruction = styled.div`
   position: absolute;
   top: 0; /* Header Height */
-  bottom: 0; /* Footer Height */
+  /* bottom: 0; Footer Height does this work here? */
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-top: 5em;
+  
 `;
-// background-image: url('/pics/truck.png');
+// background-image: url('/pics/IMG_9877.jpg');
 
 function HomePage() {
   return (
     <UnderConstruction>
-      <h1>New website coming soon!</h1>
-      <p>
-        In the meantime, please check out my{' '}
-        <a
-          href="https://www.instagram.com/begoniabaschy/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          instagram
-        </a>
-      </p>
+      <h1>Begonia Baschy Landscaping</h1>
+      {/* how do i target this h1 since its not adding padding? --Brian */}
       <ImageContainer>
-        <Truck />
+        <Family />
       </ImageContainer>
+      {/* <p>Bettina's story here?</p> */}
+      <ParagraphContainer>
+        <p>
+          Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse,
+          quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat,
+          quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio
+          dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti
+          atque corrupti, quos dolores et quas molestias excepturi sint,
+          obcaecati cupiditate non provident, similique sunt in culpa, qui
+          officia deserunt mollitia animi, id est laborum et dolorum fuga.
+        </p>
+      </ParagraphContainer>
     </UnderConstruction>
   );
 }
 
 export default HomePage;
+
