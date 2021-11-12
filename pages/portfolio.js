026c7470b8a0661dbbb2ecc913e0875a70fec582
amberwhizin <amberwhizin@gmail.com>
2021-11-12 14:55:46 -0500

    cleaned up portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 import Image from 'next/image';
-// why are the photos taking so much time to load?
+import Central from '../public/pics/CentralStreetA.jpg';
+import _5thAveB from '../public/pics/5thAveB.jpg';
+import _5thAveC from '../public/pics/5thAveC.jpg';
+import _5thAveD from '../public/pics/5thAveD.jpg';
+import _5thAveE from '../public/pics/5thAveIMG_8708.jpg';
 
 const Container = styled.div`
   position: absolute;
   top: 0;
-  /* bottom: 0; //Footer Height */
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -14,58 +17,9 @@ const Container = styled.div`
   padding: 2em;
 `;
 
-// find out why photos are loading so slow!
-const X = 3;
-const Central = () => {
-  return (
-    <Image
-      src="/pics/CentralStreetA.jpg"
-      alt="A beautiful landscape by Bettina Baschy lanscapes"
-      width={2035 / X}
-      height={1570 / X}
-    />
-  );
-};
-
-const _5thAveB = () => {
-  return (
-    <Image
-      src="/pics/5thAveB.jpg"
-      alt="A beautiful finished landscape by Bettina Baschy Lanscaping"
-      width={2035 / X}
-      height={1570 / X}
-    />
-  );
-};
-const _5thAveC = () => {
-  return (
-    <Image
-      src="/pics/5thAveC.jpg"
-      alt="A beautiful finished landscape by Bettina Baschy Lanscaping"
-      width={2035 / X}
-      height={1570 / X}
-    />
-  );
-};
-const _5thAveD = () => {
-  return (
-    <Image
-      src="/pics/5thAveD.jpg"
-      alt="A beautiful finished landscape by Bettina Baschy Lanscaping"
-      width={2035 / X}
-      height={1570 / X}
-    />
-  );
-};
-const _5thAveI = () => {
-  return (
-    <Image
-      src="/pics/5thAveIMG_8708.jpg"
-      alt="A beautiful finished landscape by Bettina Baschy Lanscaping"
-      width={2035 / X}
-      height={1570 / X}
-    />
-  );
+const PortfolioImages = ({ src, alt }) => {
+  const X = 3;
+  return <Image src={src} alt={alt} width={2035 / X} height={1570 / X} />;
 };
 
 const ImageContainer = styled.div`
@@ -77,19 +31,34 @@ const Portfolio = () => {
   return (
     <Container>
       <ImageContainer>
-        <Central />
+        <PortfolioImages
+          src={Central}
+          alt="A beautiful landscape by Bettina Baschy landscapes"
+        />
       </ImageContainer>
       <ImageContainer>
-        <_5thAveB />
+        <PortfolioImages
+          src={_5thAveB}
+          alt="A beautiful landscape by Bettina Baschy landscapes"
+        />
       </ImageContainer>
       <ImageContainer>
-        <_5thAveC />
+        <PortfolioImages
+          src={_5thAveC}
+          alt="A beautiful landscape by Bettina Baschy landscapes"
+        />
       </ImageContainer>
       <ImageContainer>
-        <_5thAveD />
+        <PortfolioImages
+          src={_5thAveD}
+          alt="A beautiful landscape by Bettina Baschy landscapes"
+        />
       </ImageContainer>
       <ImageContainer>
-        <_5thAveI />
+        <PortfolioImages
+          src={_5thAveE}
+          alt="A beautiful landscape by Bettina Baschy landscapes"
+        />
       </ImageContainer>
     </Container>
   );
