f8c0eff541c6abab028f21896a9314d9c565ea0d
amberwhizin <amberwhizin@gmail.com>
2022-06-12 13:06:31 -0400

    changes to blurb in about for client

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,7 @@
 import styled from 'styled-components';
 import Image from 'next/image';
+import BettinaAboutImg from '/public/pics/bettina_new_about_foto.jpeg';
 import { useIsTabletOrMobile } from '../hooks';
-
-const AboutTitle = `Bettina Chanel`;
-
-const AboutBlurb = `
-Hello! I’m Bettina.
-
-I’m a graduate of the Evergreen State College, where my focus was on native plants, organic farming and printmaking. Since graduating in 2013, I’ve kept my hands in the soil and followed the lead of the many wonderful mentors I’ve been lucky enough to know along the way. I’ve had the privilege to work on beautiful estates and gardens from Seattle to Olympia and between. In 2019, through the encouragement of coworkers and clients, I started my own landscaping business.\n
-
-I offer garden designs that focus on enhancing the environment for my clients as well as pollinators and other wildlife. My goal as a designer is to bring the client's personality into a garden. Whether that’s an entire rainbow of phlox and echinacea or a monochromatic moon garden. I look forward to meeting each new challenge and seeing a project through. While my specialty is plant design, I’m happy to work with other local contractors to create the space you want to spend more time in.\n
-
-When I’m not in my client’s gardens, I’m usually in mine with my toddler. When I’m home, you can find me knitting, sewing or remodeling my home and watching my favorite reality TV shows 90 Day Fiance or The Bachelor.`;
 
 const Container = styled.div`
   position: absolute;
@@ -27,59 +17,72 @@ const Container = styled.div`
   }
 `;
 
-const X = 3;
-function Family() {
+function Bettina() {
   return (
     <Image
-      src="/pics/bettina_new_about_foto.jpeg"
       alt="the landscape artist standing next to a very large green plant"
-      width={15500 / X}
-      height={19000 / X}
+      src={BettinaAboutImg}
+      width={5000}
+      height={5900}
+      placeholder="blur"
+      layout="intrinsic"
+      objectFit="cover"
+      priority
     />
   );
 }
 
 const ImageContainer = styled.div`
-  padding-left: 2rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  margin-left: 5.5rem;
+  margin-right: 20px;
 `;
 
 const TextContainer = styled.div`
-  display: block;
-  padding: 0;
-  margin: 5rem;
+  box-shadow: -0em 0 0.4em #f3f3f1, -0em 0 0.4em #f3f3f1;
+  background: linear-gradient(to left top, #fff, #f5fae3 30%, #fff);
+  margin: 7.5rem;
+  margin-right: 5.5rem;
+  margin-left: 20px;
+  font-size: 1.5rem;
+  display: flex;
+  flex-direction: column;
+
+  padding: 1rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  padding-top: 2rem;
+  line-height: 37px;
 `;
 const HeaderContainer = styled.h1`
   margin: 0;
+  padding-bottom: 10px;
   @media (max-width: 1438px) {
     text-align: center;
     margin-bottom: 0.5rem;
   }
 `;
 
-const About = () => {
+const About = ({ copy }) => {
   const isTabletOrMobile = useIsTabletOrMobile();
+  const { about = {} } = copy;
   return (
     <Container>
       {!isTabletOrMobile && (
         <>
           <ImageContainer>
-            <Family />
+            <Bettina />
           </ImageContainer>
           <TextContainer>
-            <HeaderContainer>{AboutTitle}</HeaderContainer>
-            <p>{AboutBlurb}</p>
+            <HeaderContainer>{about.title}</HeaderContainer>
+            <p>{about.blurb}</p>
           </TextContainer>
         </>
       )}
       {isTabletOrMobile && (
         <>
-          <HeaderContainer>{AboutTitle}</HeaderContainer>
-          <Family />
-          <p>{AboutBlurb}</p>
+          <HeaderContainer>{about.title}</HeaderContainer>
+          <Bettina />
+          <p>{about.blurb}</p>
         </>
       )}
     </Container>
